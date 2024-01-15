a2d0d9369b6c6f99c9a023ae33264df050bd183b
misha <mishaspring13@gmail.com>
2024-01-15 06:28:42 +0300

    remove sc

--- styled.d.ts
+++
@@ -1,8 +0,0 @@
-import 'styled-components/native';
-import theme from './src/shared/theme';
-
-type ThemeType = typeof theme;
-
-declare module 'styled-components/native' {
-  export interface DefaultTheme extends ThemeType {}
-}
